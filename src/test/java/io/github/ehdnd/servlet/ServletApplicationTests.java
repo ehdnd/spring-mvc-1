1e2b8e38cb1baa5c5db30896eab6dc40ae0006f5
Taeung Jang <155827534+ehdnd@users.noreply.github.com>
2026-07-27 13:40:21 +0000

    chore: init project

---
+++ src/test/java/io/github/ehdnd/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ehdnd.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
