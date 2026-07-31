d067be5570f676b65b881013058ccad735c6e15a
Taeung Jang <155827534+ehdnd@users.noreply.github.com>
2026-07-31 06:30:21 +0000

    4.23 회원 관리 웹 애프리케이션 요구사항

---
+++ src/test/java/io/github/ehdnd/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,49 @@
+package io.github.ehdnd.servlet.domain.member;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+// ctrl + shift + T
+class MemberRepositoryTest {
+
+  MemberRepository memberRepository = MemberRepository.getInstance();
+
+  @AfterEach
+  void afterEach() {
+    memberRepository.clearStore();
+  }
+
+  @Test
+  void save() {
+    // given
+    Member member = new Member("hello", 20);
+
+    // when
+    Member savedMember = memberRepository.save(member);
+
+    // then
+    Member findMember = memberRepository.findById(savedMember.getId());
+    assertThat(findMember).isEqualTo(findMember);
+  }
+
+  @Test
+  void findAll() {
+    // given
+    Member m1 = new Member("m1", 21);
+    Member m2 = new Member("m2", 22);
+
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+
+    // when
+    List<Member> result = memberRepository.findAll();
+
+    // then
+    assertThat(result.size()).isEqualTo(2);
+    assertThat(result).contains(m1, m2);
+
+  }
+}
