763f8fc3b71c4a81a165e4275dd79ab29cecc7cd
Taeung Jang <155827534+ehdnd@users.noreply.github.com>
2026-08-01 05:06:53 +0000

    chore: 5.33 주석 추가

--- src/main/java/io/github/ehdnd/servlet/web/frontcontroller/v3/ControllerV3.java
+++ src/main/java/io/github/ehdnd/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public interface ControllerV3 {
 
-  // 요청 파라미터 정보를 Map으로 넘기자
+  // 요청 파라미터 정보를 Map으로 넘기자 -> 파라미터 사용한 작업 진행해라
   ModelView process(Map<String, String> paramMap);
 
 }
--- src/main/java/io/github/ehdnd/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
+++ src/main/java/io/github/ehdnd/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -18,9 +18,9 @@ public class MemberSaveControllerV3 implements ControllerV3 {
     Member member = new Member(username, age);
     memberRepository.save(member);
 
+    // 파라미터 사용해 작업 한 뒤 view 에서 필요한 정보 ModelView 객체로 전달
     ModelView modelView = new ModelView("save-result");
     modelView.getModel().put("member", member);
-
     return modelView;
   }
 }
