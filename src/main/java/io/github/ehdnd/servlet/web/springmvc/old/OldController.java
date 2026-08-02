c5970118dfad52c908024d69ed44885a44e25a8b
Taeung Jang <155827534+ehdnd@users.noreply.github.com>
2026-08-02 04:59:00 +0000

    6.39 핸들러 매핑과 헨들러 어댑터
    1. 핸들러 매핑으로 핸들러 조회
    2. 핸들러 어댑터조회
    3. 핸들러 어댑터 실행

---
+++ src/main/java/io/github/ehdnd/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,22 @@
+package io.github.ehdnd.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("/springmvc/old-controller") // 스프링빈 이름을 url 패턴으로 맞추면 호출된다.
+public class OldController implements Controller {
+
+  // url 이름이랑 똑같이 생긴 스프링 빈을 찾아주는 HandlerMapping (자동등록)
+  // 핸들러 어댑터도 찾고 실행해야한다.
+
+  @Override
+  public @Nullable ModelAndView handleRequest(HttpServletRequest request,
+      HttpServletResponse response) throws Exception {
+    System.out.println("OldController.handleRequest");
+    return null;
+  }
+}
