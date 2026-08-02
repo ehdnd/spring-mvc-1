6d74ec24e08349eb26ac5c40d8180b71d7ac9e4a
Taeung Jang <155827534+ehdnd@users.noreply.github.com>
2026-08-02 05:48:30 +0000

    6.43 스프링 MVC - 실용적인 방식
    (마치 이전 v3 -> v4 처럼)

---
+++ src/main/java/io/github/ehdnd/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,48 @@
+package io.github.ehdnd.servlet.web.springmvc.v3;
+
+import io.github.ehdnd.servlet.domain.member.Member;
+import io.github.ehdnd.servlet.domain.member.MemberRepository;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+  private MemberRepository memberRepository = MemberRepository.getInstance();
+
+  //  @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+  @GetMapping("/new-form")
+  public String newForm() {
+    return "new-form";
+  }
+
+
+  //  @RequestMapping(value = "/save", method = RequestMethod.POST)
+  @PostMapping("/save")
+  public String save(@RequestParam("username") String username, @RequestParam("age") int age,
+      Model model) {
+    Member member = new Member(username, age);
+    memberRepository.save(member);
+
+    model.addAttribute("member", member);
+
+    return "save-result";
+  }
+
+  //  @RequestMapping(method = RequestMethod.GET)
+  @GetMapping
+  public String members(Model model) {
+    List<Member> members = memberRepository.findAll();
+
+    model.addAttribute("members", members);
+
+    return "members";
+  }
+}
